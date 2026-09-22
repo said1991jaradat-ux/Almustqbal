1a785fa8c0a15fd3daf084b3cb91c56272fda09b
said1991jaradat-ux <said1991jaradat@gmail.com>
2026-09-22 21:53:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors({
     origin: '*',
-    methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type']
 }));
 
